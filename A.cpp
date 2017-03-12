ea684fbf2ce847a77c1ad6e6a2fe1af97d5cd04e
redstar-ryo <e6v2e3@gmail.com>
2017-03-12 21:40:50 +0800

    code of A.cpp

--- A.cpp
+++ A.cpp
@@ -19,16 +19,17 @@ cin >> n ;
 if(n==1) 
 cout << n << endl; //題目條件如果n=1則停止 所以印出n本身
 else
-{	while(n!=1)
+{	cout << n << " ";
+	while(n!=1)
 	{
 		if(n%2==0)
 		n/=2;
 		else 
 		{n=3*n+1;}
 
-	cout<<n<<" ";
+	cout<< n <<" ";
 	}
-cout<<endl; //讓指令列出現在下一行
+cout << endl; //讓指令列出現在下一行
 }
 }
 
